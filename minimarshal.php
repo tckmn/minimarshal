de756531afbee87a2bd86320bdb114ceb3fa0242
KeyboardFire <andy@keyboardfire.com>
2014-11-16 10:31:50 -0600

    setup function done

--- minimarshal.php
+++ minimarshal.php
@@ -6,9 +6,36 @@
  * license: MIT
  */
 
-// CHANGE THIS DEFINITION AND THOSE IN THE FILE IT POINTS TO BEFORE USING MINIMARSHAL!!!
+// CHANGE THIS DEFINITION AND THOSE IN THE FILE IT POINTS TO BEFORE USING
+// MINIMARSHAL!!!
 define('_MINIMARSHAL_DEFS_PATH', 'minimarshaldefs.php');
 
-function doStuff() {
-    return "stuff";
+require_once(_MINIMARSHAL_DEFS_PATH);
+$dbh = new PDO("mysql:host=" . _MINIMARSHAL_DB_HOST . ";dbname=" .
+    _MINIMARSHAL_DB_NAME, _MINIMARSHAL_DB_USER, _MINIMARSHAL_DB_PASS);
+
+/**
+ * Run this function once and only once to initialize the MiniMarshal database
+ * @return PDOException exception object if an error occurred, NULL otherwise
+ */
+function setup() {
+    global $dbh;
+    try {
+        $dbh->exec("CREATE TABLE Pages (
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            url VARCHAR(255) NOT NULL,
+            date DATETIME NOT NULL,
+            data TEXT NOT NULL
+        );");
+        $dbh->exec("CREATE TABLE Tags (
+            id INT(11) AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL
+        );");
+        $dbh->exec("CREATE TABLE PageTags (
+            page_id INT(11) NOT NULL,
+            tag_id INT(11) NOT NULL
+        );");
+    } catch (PDOException $e) {
+        return $e;
+    }
 }
