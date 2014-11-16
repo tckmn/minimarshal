372c05edcd9c79ee0058aa40d23840f38375c4b2
KeyboardFire <andy@keyboardfire.com>
2014-11-16 08:34:55 -0600

    constants and examples

--- minimarshal.php
+++ minimarshal.php
@@ -6,6 +6,9 @@
  * license: MIT
  */
 
+// CHANGE THIS DEFINITION AND THOSE IN THE FILE IT POINTS TO BEFORE USING MINIMARSHAL!!!
+define('_MINIMARSHAL_DEFS_PATH', 'minimarshaldefs.php');
+
 function doStuff() {
     return "stuff";
 }
---
+++ minimarshaldefs.example.php
@@ -0,0 +1,6 @@
+<?php
+
+define('_MINIMARSHAL_DB_USER', 'my_username');
+define('_MINIMARSHAL_DB_PASS', 'my_password');
+define('_MINIMARSHAL_DB_HOST', 'my_database_host.example.com');
+define('_MINIMARSHAL_DB_NAME', 'minimarshal');
